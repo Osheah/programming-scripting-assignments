b1a972ef500f8836d039ab9d85a9f2bb24648d33
Helen O'Shea <35729078+Osheah@users.noreply.github.com>
2018-03-19 15:35:48 +0000

    practice problems

--- week 8/practice.py
+++ week 8/practice.py
@@ -19,8 +19,52 @@ def palindrome(s):
 '''print(palindrome('radar'))
 print(palindrome('10000011'))'''
 #3
-def simpleinterest(p,t,r):
-  ans=p+p*t*(r/100)
+def simpleinterest(p,r,t):
+  ans=p+p*t*r/100
   return ans
-print(simpleinterest(1000,3,5))
-print(simpleinterest(1000,7,10))  
+'''print(simpleinterest(1000,3,5))
+print(simpleinterest(1000,7,10))  '''
+#4
+def compoundinterest(p,r,t):
+  ans = p*(1+r/100)**t
+  return ans
+'''  
+print(compoundinterest(1000,3,5))
+print(compoundinterest(1000,7,10))'''
+#5
+def newtonsroot(n):
+  r=n/2
+  while r*r!=n:
+    r=r-(r**2-n)/(2*r)
+  return r
+'''print(newtonsroot(100))
+print(newtonsroot(144)) # still have to do it to 6 dp'''
+#6
+def pitondecs(n):
+  import math
+  ans = round(math.pi,n) # had to look up round and math pi
+  return ans
+'''print(pitondecs(2))
+print(pitondecs(6))'''
+#7
+def etondecs(n):
+  import math
+  ans = round(math.e,n) #same as 6
+  return ans
+'''print(etondecs(2))
+print(etondecs(6))'''
+#8
+def caesar(s, n):
+  alpha=[a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]
+  for i in range(len(alpha)):
+    s[i]=s[i+n]
+  return s
+print(caesar('abcd', 3))  
+print(caesar('Hello, World!', 2)) # not done
+'''
+#9
+def sortlist(l):
+  for i in range(l): 
+    if i
+#10
+def countstr(s):'''
